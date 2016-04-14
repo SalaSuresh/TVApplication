16a5b0d7520dab330345291ab14e02ca9c7bb455
Suresh Sala <Suresh Sala>
2016-04-14 11:30:08 +0530

    test comment

--- app/src/main/java/com/example/dumadu/tvapplication/MainActivity.java
+++ app/src/main/java/com/example/dumadu/tvapplication/MainActivity.java
@@ -30,5 +30,6 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //test
     }
 }
